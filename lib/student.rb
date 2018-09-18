87de481844f158c4cd3dadd0bfe6f36d0458c1dd
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-18 02:39:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,15 @@ class Student
     @id =id
   end
 
+  def self.create_table
+    sql = <<-SQL CREATE TABLE students IF NOT CREATED(
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+  end
+
 
 
 end
